ae98ee88367fe152d388875c1ab224dcce3dfc53
htphatz <huynhphatsn@gmail.com>
2025-01-16 00:09:44 +0700

    feat: build api-gateway with Spring Cloud Gateway

--- identity-service/src/main/java/com/htphatz/identity_service/controller/UserController.java
+++ identity-service/src/main/java/com/htphatz/identity_service/controller/UserController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
